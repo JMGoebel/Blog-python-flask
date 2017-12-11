6da73c2e4805b70487926c33506306af90c0465f
Jason Goebel <goebel.jason@gmail.com>
2017-12-10 23:07:26 -0600

    ADDS: Card system for previewing blog postings. WORKS

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
-
+import urllib3
+import base64
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blog_app_user:{}@jasongoebel.com:3306/blog_app_db'.format('f1nc53pM6fz0apb7ms')
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+http = urllib3.PoolManager()
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True, nullable=False)
     body = db.Column(db.Text, nullable=False)
+    image = db.Column(db.LargeBinary)
     created_on = db.Column(db.DateTime(timezone=True), server_default=func.now())
     edited_on = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, image):
         self.title = title
         self.body = body
+        self.image = image
 
 def is_valid(field):
     if field == '':
-        return "<-- This field is required."
+        return "This field is required."
 
 def sort_data(sort_on=None, sort_direction='asc'):
     if sort_on:
@@ -54,8 +58,12 @@ def newpost():
 
         if len(errors) > 0:
             return render_template('newpost.html', location="New Post",  title=post_title, body=post_body, errors=errors)
+        
+        r = http.request('GET', 'https://source.unsplash.com/400x300/?nature')
 
-        new_post = Blog(post_title, post_body)
+        image = base64.b64encode(r.data)
+        
+        new_post = Blog(post_title, post_body, image)
         db.session.add(new_post)
         db.session.commit()
 
